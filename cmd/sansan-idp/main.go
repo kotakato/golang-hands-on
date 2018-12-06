b6d85c6f61abeece90c4d35ac64d9e86944ac02b
Kota Kato <kota.kato@sansan.com>
2018-12-06 13:05:18 +0900

    Add client

---
+++ cmd/sansan-idp/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/kotakato/golang-hands-on/sansan-openapi/idp"
+)
+
+const usageTemplate = `Usage:
+
+%s [OPTIONS] (users|departments|check)
+
+Options:
+
+`
+
+func main() {
+	var (
+		endpoint string
+		apiKey   string
+	)
+
+	flag.StringVar(&endpoint, "endpoint", idp.DefaultEndpoint, "Endpoint of Sansan OpenAPI")
+	flag.StringVar(&apiKey, "api-key", "", "API key of Sansan OpenAPI")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, usageTemplate, os.Args[0])
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	if apiKey == "" {
+		fmt.Fprintf(os.Stderr, "-api-key is required\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	client := idp.NewClient(endpoint, apiKey)
+
+	switch flag.Arg(0) {
+	case "users":
+		executeUsers(client)
+	default:
+		flag.Usage()
+		os.Exit(1)
+	}
+}
+
+func executeUsers(client *idp.Client) {
+	result, err := client.GetUsers()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(result)
+}
---
+++ sansan-openapi/idp/client.go
@@ -0,0 +1,59 @@
+package idp
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+// DefaultEndpoint はデフォルトのAPIエンドポイントを表す。
+const DefaultEndpoint = "https://api.sansan.com/v2.1"
+
+// Client はSansan OpenAPIの組織APIクライアントを表す。
+type Client struct {
+	Endpoint string
+	APIKey   string
+}
+
+// NewClient はClientオブジェクトのポインタを取得する。
+func NewClient(endpoint, apiKey string) *Client {
+	if endpoint == "" {
+		endpoint = DefaultEndpoint
+	}
+	if strings.HasSuffix(endpoint, "/") {
+		endpoint = endpoint[:len(endpoint)-1]
+	}
+	return &Client{
+		Endpoint: endpoint,
+		APIKey:   apiKey,
+	}
+}
+
+// GetUsers はユーザー一覧のCSV文字列を取得する。
+func (c *Client) GetUsers() (string, error) {
+	url := c.Endpoint + "/organization/users"
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", fmt.Errorf("Failed to prepare to get users: %s", err)
+	}
+	req.Header.Set("X-Sansan-Api-Key", c.APIKey)
+
+	log.Printf("Requesting [%s] %s\n", req.Method, req.URL)
+	client := new(http.Client)
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("Failed to get users: %s", err)
+	}
+	defer resp.Body.Close()
+
+	log.Printf("Status: %d\n", resp.StatusCode)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("Failed to get users: %s", err)
+	}
+
+	return string(body), nil
+}
