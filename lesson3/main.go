c26c08de1a5caea3766caaef70cc8701e201518e
Kota Kato <kota.kato@sansan.com>
2019-01-08 13:20:27 +0900

    lesson3: 起動時にDBの接続確認をするようにした

--- lesson3/main.go
+++ lesson3/main.go
@@ -15,12 +15,14 @@ import (
 )
 
 func main() {
+	// Echoの設定
 	e := echo.New()
 	e.Logger.SetLevel(log.INFO)
 	e.Validator = web.NewEchoCustomValidator()
 	e.HTTPErrorHandler = web.EchoCustomHTTPErrorHandler
 	e.Use(middleware.Logger())
 
+	// DB接続
 	dbURL := os.Getenv("DATABASE_URL")
 	if dbURL == "" {
 		dbURL = "postgres://postgres:password@localhost/postgres?sslmode=disable"
@@ -29,12 +31,17 @@ func main() {
 	if err != nil {
 		e.Logger.Fatal(err)
 	}
+	if err := db.Ping(); err != nil {
+		e.Logger.Fatal(err)
+	}
 
+	// リポジトリとハンドラーの設定
 	repo := database.NewFilmSQLRepository(db)
 	web.SetupFilmEchoHandlers(e, repo)
-
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
+	// サーバー起動
 	e.Logger.Fatal(e.Start(":1323"))
 }
